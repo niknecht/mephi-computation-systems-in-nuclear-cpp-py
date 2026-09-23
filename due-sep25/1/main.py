e810e65003f4e509228643c102f58d8247c189a5
Nikita Vitkovskiy <nicknknecht@gmail.com>
2026-09-23 13:22:58 +0300

    feat(due-sep25): First task ready.

---
+++ due-sep25/1/main.py
@@ -0,0 +1,21 @@
+import sympy
+
+# Var 4: x + 1/x
+
+x = sympy.symbols('x', real = True)
+f = x + 1/x
+# First derivative is enough for this variant
+dfdx = sympy.diff(f, x)
+ext = sympy.solve(dfdx, x)
+
+plusDeltaX = sympy.Dummy('x', positive = True)
+viable = list(filter(lambda pt:
+                     sympy.sign(dfdx.subs(x, pt + plusDeltaX).as_leading_term(plusDeltaX).as_coeff_exponent(plusDeltaX)[0]) == 1
+                     and
+                     sympy.sign(dfdx.subs(x, pt - plusDeltaX).as_leading_term(plusDeltaX).as_coeff_exponent(plusDeltaX)[0]) == -1
+                 ,ext))
+
+print(viable)
+print(list(map(lambda pt:
+                    f.subs(x, pt)
+                ,viable)))
